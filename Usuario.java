2b46eb126614c882f08244e96ccd15462fa78276
renanadmiral <49658142+renanadmiral@users.noreply.github.com>
2019-10-07 10:34:11 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,48 @@
+
+package model.bean;
+
+
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String login;
+    private String senha;
+    
+
+    public Usuario(int id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Usuario() {
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }     
+}
